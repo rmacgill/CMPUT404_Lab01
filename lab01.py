d9dbb253e79fb1447191ce5073348617fdf4c8ea
Robert MacGillivray <rmacgill@ualberta.ca>
2021-01-11 18:22:04 -0700

    Modified to get and print from GitHub

--- lab01.py
+++ lab01.py
@@ -1,4 +1,5 @@
 import requests
 
-print(requests.__version__)
-print(requests.get("http://www.google.com"))
+#print(requests.__version__)
+#print(requests.get("http://www.google.com"))
+print(requests.get("https://raw.githubusercontent.com/rmacgill/CMPUT404_Lab01/master/lab01.py").text)
